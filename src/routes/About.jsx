4b29e4cffe7cfbb54d9baa945f00a85cc12bbf2a
Phil <natsudragoneel282003@gmail.com>
2026-07-11 15:53:48 -0700

    Created an About route and a page that descripts information about the web application

---
+++ src/routes/About.jsx
@@ -0,0 +1,15 @@
+const About = () => {
+    return (
+        <>
+            <div className="about-box">
+                Pubtopia is a web application created by Phillipe (Phil) Manio,
+                that fetches data from the Open Brewery API and displays a list
+                of breweries and pubs from all around the world. Want to explore
+                pubs in different areas or trying to find your favorite one? Pubtopia
+                is the perfect web application, just for you!
+            </div>
+        </>
+    )
+}
+
+export default About
