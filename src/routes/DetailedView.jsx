c888fa930e954ee2b6dfc9256cee93c923e3dae0
Phil <natsudragoneel282003@gmail.com>
2026-07-11 17:50:48 -0700

    Created a placeholder component for the single brewery page

---
+++ src/routes/DetailedView.jsx
@@ -0,0 +1,11 @@
+import PubDetail from '../components/PubDetail.jsx'
+
+const DetailedView = () => {
+    return (
+        <>
+            <PubDetail />
+        </>
+    )
+}
+
+export default DetailedView
