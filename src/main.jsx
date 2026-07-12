de6e13472e6de75036f81fdbce01d997a4856a4f
Phil <natsudragoneel282003@gmail.com>
2026-07-11 17:49:42 -0700

    Routes have been incorporated for navigation around the webpage

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from "react-router"
 import './index.css'
 import App from './App.jsx'
+import Layout from './routes/Layout.jsx'
+import About from './routes/About.jsx'
+import DetailedView from './routes/DetailedView.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+<BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Layout />}>
+      <Route index={true} element={<App />} />
+      <Route path="about" element={<About />} />
+      <Route path="/pubDetails/:id" element={<DetailedView/>} />
+    </Route>
+  </Routes>
+</BrowserRouter>
 )
