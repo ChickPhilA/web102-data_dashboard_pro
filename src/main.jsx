5018c1c5f22abff7b3dd581f0954e6059efb2343
Phil <natsudragoneel282003@gmail.com>
2026-07-13 14:49:53 -0700

    Added a route for pages not defined with the NotFound component.

--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from "react-router"
 import './index.css'
+
 import App from './App.jsx'
 import Layout from './routes/Layout.jsx'
 import About from './routes/About.jsx'
 import DetailedView from './routes/DetailedView.jsx'
+import NotFound from './routes/NotFound.jsx'
 
 createRoot(document.getElementById('root')).render(
 <BrowserRouter>
   <Routes>
+    {/* In this nested Route below, all child routes will have the layout present in each page. */}
     <Route path="/" element={<Layout />}>
       <Route index={true} element={<App />} />
       <Route path="about" element={<About />} />
-      <Route path="/pubDetails/:id" element={<DetailedView/>} />
+      <Route path="pubDetails/:id" element={<DetailedView/>} />
     </Route>
+    <Route path="*" element={ <NotFound /> } />
   </Routes>
 </BrowserRouter>
 )
