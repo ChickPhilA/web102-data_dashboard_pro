d377fa053d47b44dd3f29715283b03fc51216cb6
Phil <natsudragoneel282003@gmail.com>
2026-07-13 16:55:43 -0700

    Added a link to the OpenBreweryDB on the sidebar

--- src/routes/Layout.jsx
+++ src/routes/Layout.jsx
@@ -2,12 +2,13 @@
 
 import { Outlet, NavLink } from "react-router"
 
-function Layout(){
+function Layout() {
   return (
     <div className="app-shell">
       <nav className="sidebar">
         <NavLink to="/" end className="sidebar-link">🏠 Home</NavLink>
         <NavLink to="about" className="sidebar-link">ℹ️ About</NavLink>
+        <a href="https://www.openbrewerydb.org/" target="_blank" rel="noreferrer" className="sidebar-link"> 📍 API Source </a>
       </nav>
       <main className="page-content">
         <div className="page-inner">
